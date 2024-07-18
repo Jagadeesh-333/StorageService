bc1e26035833370f9a52e197f573b2c705194954
Jagadeesh Tanniru <jagadeesh.tanniru@serole.com>
2024-07-18 12:42:28 +0530

    StoreImageRep

---
+++ src/main/java/com/Jagadeesh/StoringImage/StoringImageApplication.java
@@ -0,0 +1,13 @@
+package com.Jagadeesh.StoringImage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoringImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoringImageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Jagadeesh/StoringImage/StoringImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jagadeesh.StoringImage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoringImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
